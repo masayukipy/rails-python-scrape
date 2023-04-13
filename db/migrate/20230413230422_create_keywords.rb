a08dc29eaba51cb1eeca6a78d07aaa0abcf85438
masayuki <sg.pythondev@gmail.com>
2023-04-14 00:04:54 +0100

    generate Keyword model:

---
+++ db/migrate/20230413230422_create_keywords.rb
@@ -0,0 +1,10 @@
+class CreateKeywords < ActiveRecord::Migration[7.0]
+  def change
+    create_table :keywords do |t|
+      t.string :word
+      t.integer :times
+
+      t.timestamps
+    end
+  end
+end
